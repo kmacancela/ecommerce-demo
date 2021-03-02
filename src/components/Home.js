34b5295b2055f114f9ce52f51b9881a28e03b5c3
Karina Macancela <karymacancela@gmail.com>
2021-03-02 12:21:45 -0500

    added navbar components

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,15 +8,14 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 // import MenuItem from 'react-bootstrap/MenuItem'
 
 const Home = () => {
-
     const [isOpen, setIsOpen] = useState(false);
 
     const handleOpen = () => {
-
+        setIsOpen(!isOpen);
     }
 
     const handleClose = () => {
-        
+        setIsOpen(false);
     }
 
     return(
@@ -24,23 +23,32 @@ const Home = () => {
             <Row>
                 <img className="header-logo" src="/img/belgian-boys-logo.jpg" width="100" height="100" />
                 <div className="main-menu">
-                    <Row>
-                        <div className="main-menu-btn">
-                            Shop <AiOutlineDown />
-                            <div >
-                            <ul className="menu-dropdown">
-                                <li>All Products</li>
-                                <li>Cookies</li>
-                                <li>Stroopwafels</li>
-                                <li>Breakfast</li>
-                                <li>Dessert</li>
-                            </ul>
-                            </div>
-                        </div>
+                    {/* <Row> */}
+                        {/* <div className="main-menu-btn"> */}
+                            {/* Shop <AiOutlineDown /> */}
+                            <Nav className="main-menu-btn">
+                            <Navbar.Collapse id="basic-navbar-nav">
+                                <NavDropdown 
+                                    className="menu-dropdown"
+                                    title="Shop"
+                                    id="collasible-nav-dropdown" 
+                                    show={isOpen}
+                                    onMouseEnter={handleOpen}
+                                    onMouseLeave={handleClose}
+                                >
+                                    <NavDropdown.Item href="#one">All Products</NavDropdown.Item>
+                                    <NavDropdown.Item href="#two">Cookies</NavDropdown.Item>
+                                    <NavDropdown.Item href="#three">Stroopwafels</NavDropdown.Item>
+                                    <NavDropdown.Item href="#four">Breakfast</NavDropdown.Item>
+                                    <NavDropdown.Item href="#five">Dessert</NavDropdown.Item>
+                                </NavDropdown>
+                            </Navbar.Collapse>
+                            </Nav>
+                        {/* </div> */}
                         <div className="main-menu-btn">Bundles <AiOutlineDown /></div>
                         <div className="main-menu-btn">About Us <AiOutlineDown /></div>
                         <div className="main-menu-btn">Let's Have Fun <AiOutlineDown /></div>
-                    </Row>
+                    {/* </Row> */}
                 </div>
                 <div className="secondary-menu">
                     <MdLocationOn /> 
