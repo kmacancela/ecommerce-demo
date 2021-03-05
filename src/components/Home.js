5d2e765868189cd27ff19dbf5bfb7ce1c5ba4a41
Karina Macancela <karymacancela@gmail.com>
2021-03-04 23:01:37 -0500

    navbar modify

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,32 +23,27 @@ const Home = () => {
             <Row>
                 <img className="header-logo" src="/img/belgian-boys-logo.jpg" width="100" height="100" />
                 <div className="main-menu">
-                    {/* <Row> */}
-                        {/* <div className="main-menu-btn"> */}
-                            {/* Shop <AiOutlineDown /> */}
-                            <Nav className="main-menu-btn">
-                            <Navbar.Collapse id="basic-navbar-nav">
-                                <NavDropdown 
-                                    className="menu-dropdown"
-                                    title="Shop"
-                                    id="collasible-nav-dropdown" 
-                                    show={isOpen}
-                                    onMouseEnter={handleOpen}
-                                    onMouseLeave={handleClose}
-                                >
-                                    <NavDropdown.Item href="#one">All Products</NavDropdown.Item>
-                                    <NavDropdown.Item href="#two">Cookies</NavDropdown.Item>
-                                    <NavDropdown.Item href="#three">Stroopwafels</NavDropdown.Item>
-                                    <NavDropdown.Item href="#four">Breakfast</NavDropdown.Item>
-                                    <NavDropdown.Item href="#five">Dessert</NavDropdown.Item>
-                                </NavDropdown>
-                            </Navbar.Collapse>
-                            </Nav>
-                        {/* </div> */}
-                        <div className="main-menu-btn">Bundles <AiOutlineDown /></div>
-                        <div className="main-menu-btn">About Us <AiOutlineDown /></div>
-                        <div className="main-menu-btn">Let's Have Fun <AiOutlineDown /></div>
-                    {/* </Row> */}
+                    <Nav className="main-menu-btn">
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <NavDropdown 
+                                className="menu-dropdown"
+                                title="Shop"
+                                id="collasible-nav-dropdown" 
+                                show={isOpen}
+                                onMouseEnter={handleOpen}
+                                onMouseLeave={handleClose}
+                            >
+                                <NavDropdown.Item href="#one">All Products</NavDropdown.Item>
+                                <NavDropdown.Item href="#two">Cookies</NavDropdown.Item>
+                                <NavDropdown.Item href="#three">Stroopwafels</NavDropdown.Item>
+                                <NavDropdown.Item href="#four">Breakfast</NavDropdown.Item>
+                                <NavDropdown.Item href="#five">Dessert</NavDropdown.Item>
+                            </NavDropdown>
+                        </Navbar.Collapse>
+                    </Nav>
+                    <div className="main-menu-btn">Bundles <AiOutlineDown /></div>
+                    <div className="main-menu-btn">About Us <AiOutlineDown /></div>
+                    <div className="main-menu-btn">Let's Have Fun <AiOutlineDown /></div>
                 </div>
                 <div className="secondary-menu">
                     <MdLocationOn /> 
