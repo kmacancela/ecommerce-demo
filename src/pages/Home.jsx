b98385476548beb4b3e80cf9375ed1e81593b410
Karina Macancela <karymacancela@gmail.com>
2022-01-15 17:02:04 -0500

    adding navbar and material ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,10 @@
-import styled from 'styled-components'
 import Navbar from '../components/Navbar'
-
-const Container = styled.div`
-    height: 60px;
-    background-color: black;
-    color: white;
-`;
 
 const Home = () => {
     return (
-        <Container>
+        <div>
             <Navbar />
-        </Container>
+        </div>
     )
 }
 
