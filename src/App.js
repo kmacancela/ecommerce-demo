cbec17a6882ddb1f3a8cb355c88e4594c6579203
Karina Macancela <karymacancela@gmail.com>
2022-01-15 15:24:13 -0500

    resetting app structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
+import logo from './logo.svg';
 import './App.css';
 
-import { Switch, Route } from "react-router-dom";
-import Home from './components/Home';
-
-const App = () => {
-  return(
-    <Container>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </Container>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
