82165e1eef0241d5c20c9465532e0158d84ab6ec
Karina Macancela <karymacancela@gmail.com>
2022-01-16 19:58:09 -0500

    adding footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,134 @@
+import styled from "styled-components"
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import RoomIcon from '@mui/icons-material/Room';
+import PhoneIcon from '@mui/icons-material/Phone';
+import MailOutlinedIcon from '@mui/icons-material/MailOutlined';
+
+const Container = styled.div`
+    display: flex;
+`
+
+const Left = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    padding: 20px;
+`
+
+const Logo = styled.h1`
+    
+`
+
+const Desc = styled.p`
+    margin: 20px 0;
+`
+
+const SocialContainer = styled.div`
+    display: flex;
+`
+
+const SocialIcon = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${ props => props.color };
+    margin-right: 20px;
+    cursor: pointer;
+`
+
+const Center = styled.div`
+    flex: 1;
+    padding: 20px;
+`
+
+const Title = styled.h3`
+    margin-bottom: 30px; 
+`
+
+const List = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+`
+
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`
+
+const Right = styled.div`
+    flex: 1;
+    padding: 20px; 
+`
+
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`;
+
+const Payment = styled.img`
+    width: 50%;
+`;
+
+const Footer = () => {
+    return (
+        <Container>
+            <Left>
+                <Logo>karina.</Logo>
+                <Desc>
+                    There are many variations of passages of Lorem Ipsum available, but
+                    the majority have suffered alteration in some form, by injected
+                    humour, or randomised words which don’t look even slightly believable.
+                </Desc>
+                <SocialContainer>
+                    <SocialIcon color="3B5999">
+                        <FacebookIcon />
+                    </SocialIcon>
+                    <SocialIcon color="E4405F">
+                        <InstagramIcon />
+                    </SocialIcon>
+                    <SocialIcon color="55ACEE">
+                        <TwitterIcon />
+                    </SocialIcon>
+                    <SocialIcon color="E60023">
+                        <PinterestIcon />
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Man Fashion</ListItem>
+                    <ListItem>Woman Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem><RoomIcon style={{ marginRight:"10px" }} /> 123 Main Street, New York, New York 10001</ContactItem>
+                <ContactItem><PhoneIcon style={{ marginRight:"10px" }} /> +1 347 570 5804</ContactItem>
+                <ContactItem><MailOutlinedIcon style={{ marginRight:"10px" }} /> karymacancela@gmail.com</ContactItem>
+                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+            </Right>
+        </Container>
+    )
+}
+
+export default Footer
